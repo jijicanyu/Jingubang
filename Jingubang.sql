b5886c0a58b9ce6fca8d94a47bdc0979a30d4dcf
lightless <lightless@foxmail.com>
2016-08-05 20:21:58 +0800

    update db sql

--- Jingubang.sql
+++ Jingubang.sql
@@ -1,144 +1,65 @@
--- phpMyAdmin SQL Dump
--- version 4.4.15.6
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: 2016-08-05 18:49:35
--- 服务器版本： 5.5.48-log
--- PHP Version: 5.6.22
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Database: `Jingubang`
 --
+CREATE DATABASE Jingubang DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `jingubang_admin`
+--
+
+CREATE TABLE IF NOT EXISTS `jingubang_admin` (
+  `id` SMALLINT(5) unsigned AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  `username` varchar(32) DEFAULT NULL,
+  `password` varchar(32) DEFAULT NULL,
+  `email` varchar(64) DEFAULT NULL,
+  `created_time` DATETIME DEFAULT NULL,
+  `last_login_time` DATETIME DEFAULT NULL,
+  `last_login_ip` VARCHAR(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- 表的结构 `admin`
+-- 表的结构 `jingubang_task`
 --
 
-CREATE TABLE IF NOT EXISTS `admin` (
-  `id` smallint(5) unsigned NOT NULL,
-  `username` varchar(20) NOT NULL,
-  `password` varchar(32) NOT NULL,
-  `email` varchar(320) DEFAULT NULL,
-  `mobile` varchar(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- 表的结构 `history`
---
-
-CREATE TABLE IF NOT EXISTS `history` (
-  `uid` smallint(5) unsigned NOT NULL,
-  `taskid` int(10) unsigned DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE IF NOT EXISTS `jingubang_task` (
+  `id` INTEGER(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  `uid` INTEGER(10) UNSIGNED DEFAULT NULL, # user id
+  `url` VARCHAR(512) DEFAULT NULL,          # target url
+  `inject_method` VARCHAR(20) DEFAULT NULL, # GET/POST/COOKIE/REFERER
+  `params` VARCHAR(512) DEFAULT NULL        # POST params
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- 表的结构 `Jingubang`
+-- 表的结构 `jingubang_result`
 --
 
-CREATE TABLE IF NOT EXISTS `Jingubang` (
-  `id` int(10) unsigned NOT NULL,
-  `url` varchar(1024) DEFAULT NULL,
-  `taskid` varchar(16) DEFAULT NULL,
-  `isVulnerable` tinyint(3) unsigned DEFAULT NULL,
-  `HttpMethod` varchar(20) DEFAULT NULL,
-  `banner` varchar(500) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- 表的结构 `result`
---
-
-CREATE TABLE IF NOT EXISTS `result` (
-  `taskid` int(10) unsigned NOT NULL,
-  `type` varchar(20) DEFAULT NULL,
-  `details` varchar(1024) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE IF NOT EXISTS `jingubang_result` (
+  `id` INTEGER(10) UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  `task_id` INTEGER(10) UNSIGNED DEFAULT NULL,
+  `is_inject` BOOL DEFAULT FALSE,
+  `type` varchar(64) DEFAULT NULL,
+  `payloads` varchar(1024) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- 表的结构 `user`
+-- 表的结构 `jingubang_user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` smallint(5) unsigned NOT NULL,
-  `username` varchar(20) NOT NULL,
-  `password` varchar(32) NOT NULL,
-  `email` varchar(320) DEFAULT NULL,
-  `mobile` varchar(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE IF NOT EXISTS `jingubang_user` (
+  `id` INTEGER(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  `username` varchar(32) DEFAULT NULL,
+  `password` varchar(32) DEFAULT NULL,
+  `email` varchar(64) DEFAULT NULL,
+  `created_time` DATETIME DEFAULT NULL,
+  `last_login_time` DATETIME DEFAULT NULL,
+  `last_login_ip` VARCHAR(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_general_ci;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `history`
---
-ALTER TABLE `history`
-  ADD PRIMARY KEY (`uid`);
-
---
--- Indexes for table `Jingubang`
---
-ALTER TABLE `Jingubang`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `result`
---
-ALTER TABLE `result`
-  ADD PRIMARY KEY (`taskid`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `admin`
---
-ALTER TABLE `admin`
-  MODIFY `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `Jingubang`
---
-ALTER TABLE `Jingubang`
-  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+# TODO: 考虑增加LOG表，记录注入时的log
